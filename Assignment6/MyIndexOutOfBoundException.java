567813416b6df0e2c8d7fede7cdb8772ff36ee98
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-12-11 13:51:41 -0800

    score update

--- Assignment6/MyIndexOutOfBoundException.java
+++ Assignment6/MyIndexOutOfBoundException.java
@@ -1,6 +1,6 @@
 package Assignment6;
 
-public class MyIndexOutOfBoundException extends Exception {
+public class MyIndexOutOfBoundException extends Exception { // score 2
     private int lowerBound;
     private int upperBound;
     private int index;
